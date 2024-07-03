a464860a5130f2aeb320abb7903e07e9a2f1c58c
riskyyy <theloneranger241@gmail.com>
2024-07-03 14:33:08 +0700

    add asset files

--- platform/themes/gotrip/partials/header.blade.php
+++ platform/themes/gotrip/partials/header.blade.php
@@ -1,13 +1,9 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ app()->getLocale() }}"  data-x="html" data-x-toggle="html-overflow-hidden">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=1" name="viewport"/>
-
-        <!-- Fonts-->
-        <link href="https://fonts.googleapis.com/css?family={{ urlencode(theme_option('primary_font', 'Roboto')) }}" rel="stylesheet" type="text/css">
-        <!-- CSS Library-->
 
         <style>
             :root {
